e1a25c8678fe7ed26e73e479ab839a54fff4bd5f
carloseduardoPP <carlos.caetano@picpay.com>
2023-04-19 15:12:05 -0300

    add logica de validacao

--- app/src/main/java/com/example/LoginViewModel/LoginViewModel.kt
+++
@@ -1,5 +0,0 @@
-package com.example.LoginViewModel
-
-class LoginViewModel {
-
-}
--- app/src/main/java/com/example/mylogin/LoginActivity.kt
+++ app/src/main/java/com/example/mylogin/LoginActivity.kt
@@ -1,27 +1,56 @@
 package com.example.mylogin
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import com.example.mylogin.databinding.ActitvityLoginBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.Snackbar.*
 
-class LoginActivity:AppCompatActivity(){
-    //private lateinit var binding: ActitvityLoginBinding
+open class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActitvityLoginBinding
 
-    override fun onCreate(savedInstanceState: Bundle?)  {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //binding = ActitvityLoginBinding.inflate(layoutInflater)
-        setContentView(R.layout.actitvity_login)
+        binding = ActitvityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-//        val responseName = binding.responseName
-//        val responseAge = binding.responseAge
-//        val responseEmail = binding.responseEmail
+        setLogin()
+        ScreenResult()
     }
-//
-//    fun setLogin() = with(binding){
-//        val email = tvLogin.text.toString()
-//    }
 
-    fun responseScreen(response: String){
-        val users = Users("carlos.eduardo@picpay.com","picpay123", "Carlos Eduardo", 21,"carlos.eduardo")
+    private fun setLogin() = with(binding) {
+        btnLogin.setOnClickListener() {
+            val user = etUser.text.toString()
+            val pass = etPassword.text.toString()
+
+            when {
+                user.isEmpty() -> {
+                    etUser.error = "Preencha este campo com o seu nome de usuario!"
+                }
+                pass.isEmpty() -> {
+                    etPassword.error = "Preencha este campo com a sua senha!"
+                }
+                !user.contains("@") -> {
+                    val snackbar = make(
+                        it, "Usuario inválido!",
+                        LENGTH_SHORT
+                    )
+                    snackbar.show()
+                }
+                pass.length <= 5 -> {
+                    val snackbar = make(
+                        it, "A senha precisa ter pelo menos 6 caracteres!",
+                        LENGTH_SHORT
+                    )
+                    snackbar.show()
+                }
+            }
+        }
+    }
+
+    private fun ScreenResult() = with(binding) {
+        val button = btnLogin
     }
 }
