29aa3ce50afac7c16f45b135bb6b8d27ca6be4aa
carloseduardoPP <carlos.caetano@picpay.com>
2023-04-20 11:57:51 -0300

    add logica de print na tela

--- app/src/main/java/com/example/mylogin/LoginActivity.kt
+++ app/src/main/java/com/example/mylogin/LoginActivity.kt
@@ -15,12 +15,22 @@ open class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActitvityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setLogin()
-        ScreenResult()
     }
 
     private fun setLogin() = with(binding) {
+        //DADOS
+        val usuario: String = "@carlos.eduardo"
+        val senha: String = "picpay123"
+        val nome: String = "Carlos Eduardo"
+        val idade: String = "21"
+        val email: String = "carlos.eduardo@picpay.com"
+
+        //ERROR
+        val error = "Usuario nao encontrado"
+
+
+        //VALIDACAO
         btnLogin.setOnClickListener() {
             val user = etUser.text.toString()
             val pass = etPassword.text.toString()
@@ -39,18 +49,35 @@ open class LoginActivity : AppCompatActivity() {
                     )
                     snackbar.show()
                 }
-                pass.length <= 5 -> {
+                pass.length <= 8 -> {
                     val snackbar = make(
-                        it, "A senha precisa ter pelo menos 6 caracteres!",
+                        it, "A senha precisa ter pelo menos 9 caracteres!",
+                        LENGTH_SHORT
+                    )
+                    snackbar.show()
+                }
+                pass.length > 9 -> {
+                    val snackbar = make(
+                        it, "A senha deve ter menos que 10 caracteres!",
                         LENGTH_SHORT
                     )
                     snackbar.show()
                 }
             }
-        }
-    }
+            //PRINTAR NA TELA
+            if (user == usuario && pass == senha) {
+                response.isVisible = true
+                message.isVisible = false
 
-    private fun ScreenResult() = with(binding) {
-        val button = btnLogin
+                rpName.text = nome
+                rpAge.text = idade
+                rpEmail.text = email
+            } else if (user != usuario && pass != senha) {
+                message.isVisible = true
+                response.isVisible = false
+
+                tvMessage.text = "Usuario nao encontrado!"
+            }
+        }
     }
 }
--- app/src/main/java/com/example/mylogin/Users.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mylogin
-
-data class Users(
-    val email:String,
-    val password:String,
-    val name:String,
-    val age:Int,
-    val nameUser:String
-)
